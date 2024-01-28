c96c5d27a875cd7bf3c75d6c6e7aaf09534adfd3
Alana Herbin-Debrincat <aherbin@student.42berlin.de>
2024-01-28 03:26:57 +0100

    hehe

--- Client_SRC/client.c
+++ Client_SRC/client.c
@@ -6,8 +6,24 @@
 /*   By: aherbin <aherbin@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 17:35:25 by aherbin           #+#    #+#             */
-/*   Updated: 2024/01/26 17:35:38 by aherbin          ###   ########.fr       */
+/*   Updated: 2024/01/26 18:03:06 by aherbin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "client.h"
+
+static int	ft_send(__pid_t srv_pid, char *str)
+{
+
+}
+
+int	main(int argc, char **argv)
+{
+	__pid_t	srv_pid;
+//	PID =  argv[1]
+//	str_to_send = argv[2]
+	if (argc != 3)
+		return (-1);
+	srv_pid = ft_atoi(argv[1]);
+	return (ft_send(srv_pid, argv[2]));
+}
--- Server_SRC/server.c
+++ Server_SRC/server.c
@@ -6,8 +6,24 @@
 /*   By: aherbin <aherbin@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 17:36:14 by aherbin           #+#    #+#             */
-/*   Updated: 2024/01/26 17:36:31 by aherbin          ###   ########.fr       */
+/*   Updated: 2024/01/26 17:59:12 by aherbin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "server.h"
+
+static void listen(void)
+{
+
+}
+
+int	main(void)
+{
+	__pid_t	pid;
+
+	ft_printf("Process Id = %i\n", pid);
+	while (1)
+		listen();
+	return (0);
+}
+
