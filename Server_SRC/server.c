8bfa0b8d933a056bbe5c9ddc5cb8557fd5c67ea9
Alana Herbin <aherbin@student.42berlin.de>
2024-01-29 20:30:35 +0100

    Some bugs fixed

--- Server_SRC/server.c
+++ Server_SRC/server.c
@@ -6,7 +6,7 @@
 /*   By: aherbin <aherbin@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 17:36:14 by aherbin           #+#    #+#             */
-/*   Updated: 2024/01/29 17:41:50 by aherbin          ###   ########.fr       */
+/*   Updated: 2024/01/29 20:29:30 by aherbin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,15 +33,19 @@ by: aherbin\n\n\033[0m", pid);
 }
 */
 
-//void	*signal_handler(int signum)
-//{
-//	ft_printf("received signum %i\n", signum);
-//}
+void	sig_handler(int signum)
+{
+	ft_printf("received signum %i\n", signum);
+	return;
+}
 
 int	main(void)
 {
 	ft_print_header((int) getpid());
+	signal(SIGUSR1, sig_handler);
 	pause();
+	//while (1)
+	//	sleep(1);
 	//sigaction(SIGUSR1, &signal_handler, NULL);
 	//ft_printf("Process Id = %i\n", pid);
 	//while (1)
